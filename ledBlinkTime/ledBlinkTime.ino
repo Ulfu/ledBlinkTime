41f3fa7036c0e0f7f38cf86e0aee460578d6d035
Ulfu <din_dr_elvis@live.se>
2016-10-03 13:34:08 +0200

    Added some comments for school assignments

--- ledBlinkTime/ledBlinkTime.ino
+++ ledBlinkTime/ledBlinkTime.ino
@@ -1,31 +1,28 @@
 /*
-  Button
 
  Turns on and off a light emitting diode(LED) connected to digital
- pin 13, when pressing a pushbutton attached to pin 2.
+ pin 13. Change the amount of time the LED is on and off when you press a button.
 
 
  The circuit:
  * LED attached from pin 13 to ground
  * pushbutton attached to pin 2 Pullupp-switch
 
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
-
- This example code is in the public domain.
+ This code is in the public domain.
  */
 
-// constants won't change. They're used here to
-// set pin numbers:
-const int addButtonPin = 2;     // the number of the pushbutton pin
-const int deButtonPin = 3;
+// constants won't change.
+// Seting pin numbers:
+const int addButtonPin = 2;     // the number of the pushbutton pin for the increase blinktime button
+const int deButtonPin = 3;    // the number of the pushbutton pin for the decrease blinktime button
 const int ledPin =  13;      // the number of the LED pin
 
+// these variables can change
 boolean addButtonValue = true;   //Current add button value
 boolean deButtonValue = true; //Current decrease button value
 int ledBlinkTime = 0; //Current time to wait before next led blink
 
-void setup() {
+void setup() { // Setup runs a single time
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
@@ -34,20 +31,20 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
-  // read the state of the pushbutton value, write the opposite value to ledPin:
+void loop() { // loop runs forever, basically an infinite while loop
+  // read the state of the pushbutton value, write the value to its respective variable:
   addButtonValue = digitalRead(addButtonPin); //Copy button value to the variable
   deButtonValue = digitalRead(deButtonPin); //Copy button value to the variable
   if (!addButtonValue) { //Check if the button is pressed
-      ledBlinkTime += 1; //add pwm value
+      ledBlinkTime += 10; //add delay value
     }
   if (!deButtonValue) { //Check if the button is pressed
-      ledBlinkTime -= 1; //de pwm value
+      ledBlinkTime -= 10; //decrease delay value
     }
-  ledBlinkTime = constrain(ledBlinkTime, 0, 30000);
+  ledBlinkTime = constrain(ledBlinkTime, 0, 30000); // Constrain ledBlinkTime between 0 and 30000
   digitalWrite(ledPin, HIGH); //Write the values
-  delayMicroseconds(ledBlinkTime*2);  //Pause the program for ledBlinkTime
+  delayMicroseconds(ledBlinkTime*2);  //Pause the program for ledBlinkTime times 2
   digitalWrite(ledPin, LOW); //Write the values
-  delayMicroseconds(ledBlinkTime*2);  //Pauses for the time ledBlinkTime
+  delayMicroseconds(ledBlinkTime*2);  //Pauses for the time ledBlinkTime times 2
   Serial.println(ledBlinkTime); //Pause the program for ledBlinkTime
 }
